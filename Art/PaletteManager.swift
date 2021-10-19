b0cdd37c8b89d5487481b96c2fb9c8c99d82c250
Daichi Morihara <daichi16daichi04@icloud.com>
2021-10-19 15:42:45 +0900

    add some features

--- Art/PaletteManager.swift
+++ Art/PaletteManager.swift
@@ -9,20 +9,41 @@ import SwiftUI
 
 struct PaletteManager: View {
     @EnvironmentObject var store: PaletteStore
+    
+    @State private var editMode: EditMode = .inactive
+    @Environment (\.presentationMode) var presentationMode
+
     var body: some View {
         NavigationView {
             List {
                 ForEach(store.palettes) { palette in
                     NavigationLink(destination: PaletteEditor(palette: $store.palettes[palette])) {
                         VStack(alignment: .leading) {
-                            Text(palette.name)
+                            Text(palette.name).font(editMode == .active ? .largeTitle : .caption)
                             Text(palette.emojis)
                         }
                     }
                 }
+                .onDelete { indexSet in
+                    store.palettes.remove(atOffsets: indexSet)
+                }
+                .onMove { indexSet, newSet in
+                    store.palettes.move(fromOffsets: indexSet, toOffset: newSet )
+                }
             }
             .navigationTitle("Manage Palettes")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem { EditButton() }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    if presentationMode.wrappedValue.isPresented {
+                        Button("Close") {
+                            presentationMode.wrappedValue.dismiss()
+                        }
+                    }
+                }
+            }
+            .environment(\.editMode, $editMode)
         }
     }
 }
